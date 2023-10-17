fe9af83e6599ad0b8355a7fad5da966213b1d191
youssefAbooooo <youssef@example.com>
2023-10-17 20:08:51 +0300

    handle struct with betty

--- main.h
+++ main.h
@@ -4,12 +4,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+
 int _putchar(char c);
 int _strlen(char *s);
 int _printf(const char *format, ...);
 
-
-
+/**
+ * struct specefier - this struct specefies which function will be executed
+ * @c: char
+ * @P: pointer function
+ *
+ * Description: this struct specefies which function will be executed
+ */
 
 
 typedef struct specefier
