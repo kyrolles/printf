a0bd5de192a1711247f5bece083a4b8db2aab00c
youssefAbooooo <youssef@example.com>
2023-10-17 22:20:19 +0300

    small rat y

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
 * _printf - this function print a sentences and can print two type
 * of variable and the '%'.
@@ -9,26 +8,24 @@
 *
 * Return: count.
 */
-
 int _printf(const char *format, ...)
 {
-spec fun[] = {{'c', print_char}, {'s', print_string}, {'%', print_precent}, {'d', print_decimal}, {'i', print_decimal}};
 int x = 0;
 int i = 0;
-int flag = 0;
+int f = 0;
+int cs = 0;
+int c = 0;
 unsigned long  r;
-int count_s = 0;
-int count = 0;
+spec fun[] = {{'c', print_char}, {'s', print_string}, {'%', print_precent}}
 
 va_list args;
 va_start(args, format);
-
 while (format[i])
 {
 if (format[i] != '%')
 {
 _putchar(format[i]);
-count_s++;
+cs++;
 }
 else
 {
@@ -36,21 +33,18 @@ for (r = 0; r < (sizeof(fun) / sizeof(fun[0])); r++)
 {
 if (format[i + 1] == fun[r].c)
 {
-flag = 1;
+f = 1;
 x = x + fun[r].P(args);
 i++;
 break;
 }
 }
-if(flag == 0)
+if (f == 0)
 _putchar('%');
 }
 i++;
 }
-
-
-count = count_s + x;
+c = cs + x;
 va_end(args);
-
-return (count);
+return (c);
 }
