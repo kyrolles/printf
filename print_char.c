b4d8633547008edeba9ac5d31614f4d05e91908f
youssefAbooooo <youssef@example.com>
2023-10-17 14:00:11 +0300

    3 files updated Y

--- print_char.c
+++ print_char.c
@@ -1,9 +1,15 @@
-#include "main.h" 
+#include "main.h"
+
+/**
+ * print_char - this function print's a single char
+ * @args: this a number
+ *
+ * Return: Always 1 (Success)
+ */
 
 int print_char(va_list args)
 {
-char c = va_arg(args, int); 
+char c = va_arg(args, int);
 _putchar(c);
-
 return (1);
 }
